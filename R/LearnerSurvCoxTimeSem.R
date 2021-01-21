c96bb389f6fb8443f39806648aeb5d51dd890c92
simonwiegrebe1992 <simon.wiegrebe@gmail.com>
2021-01-21 17:03:34 +0100

    only allow same #neurons for all layers, make #neuron search space discrete

--- R/LearnerSurvCoxTimeSem.R
+++ R/LearnerSurvCoxTimeSem.R
@@ -142,7 +142,7 @@ LearnerSurvCoxtime2 = R6::R6Class("LearnerSurvCoxtime2",
                                      # Set-up network architecture
                                      # num_nodes needs to be reconstructed
                                      num_nodes <- rep(as.integer(as.integer(self$param_set$get_values(tags = "net")$nodes_per_layer)),
-                                                      self$param_set$get_values(tags = "net")$num_layers),
+                                                      self$param_set$get_values(tags = "net")$num_layers)
                                      # num_nodes_raw = c(self$param_set$get_values(tags = "net")$num_nodes1,
                                      #                   self$param_set$get_values(tags = "net")$num_nodes2,
                                      #                   self$param_set$get_values(tags = "net")$num_nodes3,
